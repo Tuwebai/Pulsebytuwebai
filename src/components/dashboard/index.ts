cbc4b427ee096df125669e553e120e81d86f1a0c
Tuwebai <tuwebai@gmail.com>
2026-03-28 20:00:02 -0300

    chore(legacy): borrar componentes muertos fuera de admin

--- src/components/dashboard/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all dashboard components
-export { DraggableWidget } from './DraggableWidget';
-export type { Widget } from './DraggableWidget';
-
-export { DashboardGrid } from './DashboardGrid';
-export type { DashboardView } from './DashboardGrid';
-
-export { ViewManager } from './ViewManager';
--- src/components/filters/index.ts
+++
@@ -1,2 +0,0 @@
-// Export all filter components
-export { FilterPanel } from './FilterPanel';
